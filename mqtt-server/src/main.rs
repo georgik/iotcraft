02ae63cb05c0c790c307668b57ad1fd5103ad07d
Juraj Michálek <juraj.michalek@gmail.com>
2025-07-13 16:36:47 +0200

    mqtt server: log information about interfaces on boot

--- mqtt-server/src/main.rs
+++ mqtt-server/src/main.rs
@@ -3,7 +3,7 @@ use tracing_subscriber;
 use tracing::info;
 use config::Config as FileConfig;
 use config::File as ConfigFile;
-use rumqttd::{Broker, Config, Notification};
+use rumqttd::{Broker, Config};
 
 fn main() {
     // initialize logging
@@ -16,6 +16,24 @@ fn main() {
         .unwrap();
     let rumq_cfg: Config = file_cfg.try_deserialize().unwrap();
     let mut broker = Broker::new(rumq_cfg);
+
+    // Log the IP addresses on which the server is binding
+    info!("MQTT Server configured to bind to 0.0.0.0, available IP addresses:");
+    if let Ok(ifaces) = if_addrs::get_if_addrs() {
+        for iface in &ifaces {
+            if !iface.is_loopback() {
+                info!("  - {} ({})", iface.addr.ip(), iface.name);
+            }
+        }
+        // Also log loopback for completeness
+        for iface in &ifaces {
+            if iface.is_loopback() {
+                info!("  - {} ({}) [loopback]", iface.addr.ip(), iface.name);
+            }
+        }
+    } else {
+        info!("Could not retrieve network interfaces.");
+    }
 
     // create a link to receive broker notifications
     let (mut _link_tx, mut link_rx) = broker.link("mqtt-server").unwrap();
